7cf58379c7960775ddeffabb040f3e92397f18bf
Brandon Burnett <gentoolicious@gmail.com>
2015-04-13 00:28:57 +0000

    Adding salt::api spec classes.

---
+++ spec/classes/api_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe 'salt::api', :type => 'class' do
+
+  ['Debian', 'RedHat', 'SUSE', 'ArchLinux' ].each do |distro|
+    context "on #{distro}" do
+      let(:facts) {{
+          :osfamily => distro,
+          :concat_basedir => '/tmp'
+      }}
+
+      [ 'cherrypy', 'tornado', 'wsgi' ].each do |api|
+        context "with the #{api} api enabled" do
+          let (:params) { { "api_enable_#{api}".to_sym => true } }
+
+          it { should contain_package('salt-api') }
+
+          it { should contain_service('salt-api').with(
+            'ensure'     => 'running',
+            'enable'     => 'true',
+            'hasstatus'  => 'true',
+            'hasrestart' => 'true'
+          )}
+
+          it { should contain_concat__fragment('api')
+            .with({ :target => '/etc/salt/master'})
+          }
+
+          it { should contain_concat('/etc/salt/master')
+            .with_content(/^rest_#{api}:$/)
+          }
+
+
+        end
+      end
+
+    end
+  end
+end
---
+++ spec/classes/master_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe 'salt::master', :type => 'class' do
+
+  ['Debian', 'RedHat', 'SUSE', 'ArchLinux' ].each do |distro|
+    context "on #{distro}" do
+      let(:facts) {{
+          :osfamily => distro,
+          :concat_basedir => '/tmp'
+      }}
+
+      [ 'cherrypy', 'tornado', 'wsgi' ].each do |api|
+        context "with the #{api} api enabled" do
+          let(:params) { { "api_enable_#{api}".to_sym => true } }
+
+          it { should_not contain_file('/etc/salt/puppet') }
+          it { should contain_concat__fragment('master')
+            .with({ :target => '/etc/salt/master'})
+          }
+        end
+      end
+
+    end
+  end
+end
